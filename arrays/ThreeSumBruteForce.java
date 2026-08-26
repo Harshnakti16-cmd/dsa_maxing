2c97553f6fc33ef3427aef761819f35f64ddecc8
Harshnakti16-cmd <harshnakti0@gmail.com>
2026-08-26 17:31:58 +0530

    leetcode 15 bruteforce approach

---
+++ arrays/ThreeSumBruteForce.java
@@ -0,0 +1,26 @@
+public class ThreeSumBruteForce {
+
+    public static int[] threeSum(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        return new int[] { arr[i], arr[j], arr[k] };
+                    }
+                }
+            }
+        }
+        return new int[] { -1, -1, -1 };
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, -2, 3, 4, -6, 2 };
+
+        int[] result = threeSum(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i] + " ");
+        }
+    }
+}
