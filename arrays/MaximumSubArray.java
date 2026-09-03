4dd41df0cdf7aa22d8110c0b5e5c9570b836bfc1
Harshnakti16-cmd <harshnakti0@gmail.com>
2026-09-03 12:04:57 +0530

    fixed the edge case

--- arrays/MaximumSubArray.java
+++ arrays/MaximumSubArray.java
@@ -24,10 +24,10 @@ public class MaximumSubArray {
 
     public static int plus(int[] arr) {
 
-        int currentSum = 0;
-        int maxSum = 0;
+        int currentSum = arr[0];
+        int maxSum = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             currentSum = Math.max(arr[i], currentSum + arr[i]);
 
             maxSum = Math.max(maxSum, currentSum);
