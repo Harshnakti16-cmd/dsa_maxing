41dcdca26379b624de7e2e0a23c32f3c66137e96
Harshnakti16-cmd <harshnakti0@gmail.com>
2026-09-04 16:09:04 +0530

    added description for 344

--- strings/ReverseString.java
+++ strings/ReverseString.java
@@ -1,11 +1,27 @@
+/**
+ * LeetCode 344: Reverse String
+ * 
+ * Problem Description:
+ * Write a function that reverses a string. The input string is given as an
+ * array of characters char[].
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array
+ * in-place with O(1) extra memory.
+ * 
+ * Approach:
+ * Two Pointers technique -Swap characters starting from two ends (left and
+ * right) moving towards center.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
 public class ReverseString {
-    
-    public static void reverse(char[] ch){
+
+    public static void reverse(char[] ch) {
 
         int left = 0;
-        int right = ch.length -1;
+        int right = ch.length - 1;
 
-        while(left < right){
+        while (left < right) {
 
             char temp = ch[left];
             ch[left] = ch[right];
@@ -14,14 +30,14 @@ public class ReverseString {
             left++;
             right--;
         }
-        for( char c : ch ){
+        for (char c : ch) {
             System.out.print(c + " ");
         }
     }
-    
-    public static void main(String[] args){
 
-        char[] ch = {'h','a','r','s','h'};
+    public static void main(String[] args) {
+
+        char[] ch = { 'h', 'a', 'r', 's', 'h' };
         reverse(ch);
     }
 }
